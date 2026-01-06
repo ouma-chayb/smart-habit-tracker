6c76012a5daa60d3487f191feafc0a9141146c1b
ouma-chayb <omaymashayb@gmail.com>
2026-01-06 23:56:27 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e
 
 cd /home/ec2-user/smart-habit-tracker
 
-python3 -m venv venv
+# Installer python si besoin
+sudo yum install -y python3
+
+# Créer venv si inexistant
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
-nohup python3 app.py > app.log 2>&1 &
+# Tuer l'ancien serveur
+pkill -f app.py || true
+
+# Lancer Flask
+nohup python app.py > app.log 2>&1 &
